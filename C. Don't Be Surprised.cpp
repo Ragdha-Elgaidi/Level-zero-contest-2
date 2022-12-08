ae0a54a64f4a31bd7f2c28b33719ca570af5f099
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-08 02:58:45 +0200

    Create C. Don't Be Surprised.cpp

---
+++ C. Don't Be Surprised.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double R = 0.0;
+
+    cin >> R;
+    cout << fixed << setprecision(6)<< pow(2*R, 2) << endl;
+    return 0;
+}
